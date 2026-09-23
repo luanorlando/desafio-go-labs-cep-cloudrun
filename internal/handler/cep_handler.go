cf5fc4eec63a94cd0cf00810f11860d401f58253
luanorlando <orlandoluan@hotmail.com>
2026-09-23 00:22:58 -0300

    fix: remove unused import

--- internal/handler/cep_handler.go
+++ internal/handler/cep_handler.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
-	"strings"
 
 	"github.com/luanorlando/desafio-go-labs-cep-cloudrun.git/internal/entity"
 	"github.com/luanorlando/desafio-go-labs-cep-cloudrun.git/internal/usecase"
@@ -21,14 +20,14 @@ func NewHandler(u *usecase.FetchCEPUsecase) *CEPHandler {
 }
 
 func (h *CEPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	params := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
+	// params := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
 
-	if len(params) != 2 || params[0] == "" {
-		http.Error(w, "CEP não encontrado na url", http.StatusBadRequest)
-		return
-	}
+	// if len(params) != 2 || params[0] == "" {
+	// 	http.Error(w, "CEP não encontrado na url", http.StatusBadRequest)
+	// 	return
+	// }
 
-	cep := params[1]
+	cep := r.PathValue("cep")
 
 	result, err := h.usecase.Execute(cep)
 
